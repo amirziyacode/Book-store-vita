33676295942a542d3ac68b61344156a9591455c7
amirziyacode <amzdidi@gmail.com>
2025-02-26 21:32:45 +0330

    home page

---
+++ src/componets/home..jsx
@@ -0,0 +1,43 @@
+import React from "react"
+import "../css/home.css";
+function Home() {
+    return (
+        <>
+        <nav>
+            <h2>Book Store</h2>
+            <ul className="nav-ite">
+                <li>BestSellers</li>
+                <li>BookStroes</li>
+                <li>Login</li>
+            </ul>
+            <button class="btn-outline"><p class="text-btn">Sing Up</p></button>
+        </nav>
+        <div className="home-body">
+            <div className="home-text-btn">
+                <p className="big-text-gray">
+                    Let's celebrate together
+                </p>
+                <p className="big-text-black">
+                    Happ World Book Day
+                </p>
+                <p className="dcp">
+                    Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+                    Harum fugiat, rem laborum accusamus est aliquid atque. Culpa fugit incidunt a magnam eius ullam debitis magni,
+                    nemo veritatis quod nihil. In?
+                </p>
+                <button className="btn-primary">
+                    Get Started
+                </button>
+                <button className="btn-primary-outline">
+                    Discover
+                </button>
+            </div>
+            <div className="img">
+                <img src="src/assets/book-landing-page.png" alt="book-store" className="img-book" />
+            </div>
+        </div>
+        </>
+    )
+}
+
+export default Home;
